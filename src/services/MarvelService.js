ef79570b02f39da051746652890f9503e656f17c
wintermute28 <a28c@yandex.ru>
2023-12-18 22:28:26 +0300

    Add random character section

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,12 +13,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResurce(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResurce(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResurce(`${this._apiBase}characters/${id}?&apikey=${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResurce(`${this._apiBase}characters/${id}?&apikey=${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    } 
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
