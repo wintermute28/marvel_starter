dc87012c79f58188a29a80fbfaecc025379c4ed2
wintermute28 <a28c@yandex.ru>
2024-02-06 16:06:06 +0300

    Minor bug fixes

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,5 +1,3 @@
-
-
 class MarvelService {
     _apiBase = "https://gateway.marvel.com:443/v1/public/";
     _apiKey = process.env.REACT_APP_MARVEL;
@@ -14,12 +12,12 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResurce(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
+        const res = await this.getResurce(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
     getCharacter = async (id) => {
-        const res = await this.getResurce(`${this._apiBase}characters/${id}?&apikey=${this._apiKey}`);
+        const res = await this.getResurce(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     } 
 
