067c78193c795b29fb15c2091e731a1ae1deb3b6
wintermute28 <a28c@yandex.ru>
2024-02-19 17:20:21 +0300

    Use React-Router

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+
+import decoration from '../../resources/img/vision.png';
+
+
+const MainPage = () => {
+
+    const [selectedChar, setChar] = useState(null);
+
+    const onChaeSelected = (id) => {
+        setChar(id);
+    }
+
+  return (
+    <>
+        <ErrorBoundary>
+            <RandomChar/>
+        </ErrorBoundary>
+        <div className="char__content">
+            <ErrorBoundary>
+                <CharList onChaeSelected={onChaeSelected}/>
+            </ErrorBoundary>
+            <ErrorBoundary>
+                <CharInfo charId={selectedChar}/>
+            </ErrorBoundary> 
+        </div>
+        <img
+            className="bg-decoration"
+            src={decoration}
+            alt="vision"/>
+    </>
+  )
+}
+
+export default MainPage;
